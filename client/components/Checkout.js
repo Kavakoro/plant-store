37c2db90365de49ce95a65a32d5ea6364e333ef3
Kokko Tso <kokkotso@gmail.com>
2021-02-12 09:47:57 -0500

    Create form input components and hook up to order processing logic

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import { checkout } from "../store/cart";
 
 import axios from "axios";
 
@@ -16,7 +18,28 @@ class Checkout extends React.Component {
 				const subTotal = curr.price * plantAmount;
 				return acc + subTotal;
 			}, 0),
+			address: {
+				street: "",
+				city: "",
+				state: "",
+				zip: "",
+			},
 		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(ev) {
+		this.setState({
+			address: { ...this.state.address, [ev.target.name]: ev.target.value },
+		});
+	}
+
+	handleSubmit() {
+		console.log("clicked");
+		const _address = `${this.state.address.street} ${this.state.address.city}, ${this.state.address.state} ${this.state.address.zip}`;
+		const _orderId = this.props.cart.id;
+		this.props.confirmPurchase(_orderId, _address);
 	}
 
 	async componentDidMount() {
@@ -48,10 +71,40 @@ class Checkout extends React.Component {
 						);
 					})}
 				</ul>
-				<div>
-					<h2>Order Total: {this.state.totalPrice}</h2>
-				</div>
-				<Button variant="contained">Confirm Purchase</Button>
+				<h2>Order Total: {this.state.totalPrice}</h2>
+				<form>
+					<Input
+						type="text"
+						name="street"
+						placeholder="Street Address"
+						onChange={this.handleChange}
+						value={this.state.address.street}
+					/>
+					<Input
+						type="text"
+						name="city"
+						placeholder="City"
+						onChange={this.handleChange}
+						value={this.state.address.city}
+					/>
+					<Input
+						type="text"
+						name="state"
+						placeholder="State"
+						onChange={this.handleChange}
+						value={this.state.address.state}
+					/>
+					<Input
+						type="text"
+						name="zip"
+						placeholder="ZIP Code"
+						onChange={this.handleChange}
+						value={this.state.address.zip}
+					/>
+				</form>
+				<Button variant="contained" onClick={this.handleSubmit}>
+					Confirm Purchase
+				</Button>
 			</div>
 		);
 	}
@@ -63,8 +116,10 @@ const mapState = (state) => {
 	};
 };
 
-const mapDispatch = () => {
-	return {};
+const mapDispatch = (dispatch) => {
+	return {
+		confirmPurchase: (orderId, address) => dispatch(checkout(orderId, address)),
+	};
 };
 
 export default connect(mapState, mapDispatch)(Checkout);
