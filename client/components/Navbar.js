def6e499272584a04a6557447dc0a7cebee37c75
Kate Quinn <katequinn7@gmail.com>
2021-02-12 23:47:32 -0500

    changed navbar logic and updated navbar css

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,81 +1,86 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { logout } from "../store";
-import AppBar from "@material-ui/core/AppBar";
-import "../../public/Navbar.css";
-import { createMuiTheme } from "@material-ui/core/styles";
-import { ThemeProvider } from "@material-ui/styles";
-import { green } from "@material-ui/core/colors";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../store';
+import AppBar from '@material-ui/core/AppBar';
+import '../../public/Navbar.css';
+import { fetchCart } from '../store/cart';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { green } from '@material-ui/core/colors';
 // import { yellow } from "@material-ui/core/colors";
-import Button from "@material-ui/core/Button";
+import Button from '@material-ui/core/Button';
 
 const navBarTheme = createMuiTheme({
   palette: {
     primary: {
       // Purple and green play nicely together.
-      main: green[900],
+      main: '#224229',
     },
   },
 });
 
-const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div>
-    <ThemeProvider theme={navBarTheme}>
-      <AppBar position="static" color="primary" style={{ height: "6rem" }}>
-        {/* <nav>
-        <div id="navbar">
-          <div>
-            <h1>KaVaKoRo's Plants</h1>
-          </div>
-          <div id="nav-links"></div>
-          {isLoggedIn ? (
-            <>
-              <Link to="/home">Home</Link>
-              <a href="/home" onClick={handleClick}>
-                Logout
-              </a>
-            </>
-          ) : (
-            <div>
-              Returning customer?
-              <Link to="/login">Login</Link>
-            </div>
-          )}
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/cart">Cart</Link>
-        </div>
-      </nav> */}
-        <nav>
-          <div id="navbar">
-            <div>
-              <Link to="/home">
-                <h1>Kavakoro's Plants</h1>
-              </Link>
-            </div>
-            <div id="nav-links">
-              {/* <Link to="/home">Home</Link> */}
-              {isLoggedIn ? (
-                <a href="/home" onClick={handleClick}>
-                  Logout
-                </a>
-              ) : (
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  async componentDidMount() {
+    const orderId = window.localStorage.getItem('orderId');
+    if (orderId) {
+      this.props.getCart(orderId);
+    } else {
+      window.localStorage.setItem('orderId', orderId);
+      this.props.getCart(orderId);
+    }
+  }
+  render() {
+    const { handleClick, isLoggedIn } = this.props;
+    return (
+      <div>
+        <ThemeProvider theme={navBarTheme}>
+          <AppBar
+            position="static"
+            color="primary"
+            style={{
+              height: '6rem',
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <nav>
+              <div id="navbar">
                 <div>
-                  Returning customer?
-                  <Link to="/login">Login</Link>
+                  <Link id="home" to="/home">
+                    <h1>Kavakoro's Plants</h1>
+                  </Link>
                 </div>
-              )}
-              <Link to="/signup">Sign Up</Link>
-              <Link to="/cart">Cart</Link>
-              <Link to="/admin">Admin</Link>
-            </div>
-          </div>
-        </nav>
-        <hr></hr>
-      </AppBar>
-    </ThemeProvider>
-  </div>
-);
+                <div id="nav-links">
+                  {/* <Link to="/home">Home</Link> */}
+                  {isLoggedIn ? (
+                    <a href="/home" onClick={handleClick}>
+                      Logout
+                    </a>
+                  ) : (
+                    <div>
+                      <span>Returning customer?</span>
+                      <Link to="/login">Login</Link>
+                    </div>
+                  )}
+                  <Link to="/signup">Sign Up</Link>
+                  <Link to="/cart">
+                    <img height="40" width="35" src="/images/cart.png"></img>
+                  </Link>
+                  <Link to="/admin">Admin</Link>
+                </div>
+              </div>
+            </nav>
+            <hr></hr>
+          </AppBar>
+        </ThemeProvider>
+      </div>
+    );
+  }
+}
 
 /**
  * CONTAINER
@@ -91,6 +96,7 @@ const mapDispatch = (dispatch) => {
     handleClick() {
       dispatch(logout());
     },
+    getCart: (orderId) => dispatch(fetchCart(orderId)),
   };
 };
 
