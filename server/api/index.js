09c134eb634c86cbd738ee75c37acaf6d025ce79
Kokko Tso <kokkotso@gmail.com>
2021-02-14 20:19:48 -0500

    Add dynamic orderId for testing purposes

--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,16 @@
-const router = require('express').Router();
+const router = require("express").Router();
 module.exports = router;
 
-router.use('/users', require('./users'));
-router.use('/plants', require('./plants'));
-router.use('/cart', require('./cart'));
-router.use('/orders', require('./orders'));
+router.use("/users", require("./users"));
+router.use("/plants", require("./plants"));
+router.use("/cart", require("./cart"));
+router.use("/orders", require("./orders"));
+
+// for testing purposes only - delete before deploying to production
+router.use("/test", require("./test"));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found');
-  error.status = 404;
-  next(error);
+	const error = new Error("Not Found");
+	error.status = 404;
+	next(error);
 });
