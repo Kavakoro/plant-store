4e4d30e67916892b4877a1a76b93cc33083be945
Rommel Demano <youremail>
2021-02-19 18:01:16 -0500

    updated singlePlant, admin css, shipping/return

--- client/components/ReturnPolicy.js
+++ client/components/ReturnPolicy.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../../public/ReturnPolicy.css";
+import { Link } from "react-router-dom";
 
 const ReturnPolicy = () => {
   return (
     <div className="return">
       <h1>Return Policy</h1>
       <br />
-      <div>
+      <div id="return">
         <h2>Can you return plants? No.</h2>
         <p>
           At this time, Kavakoro does not accept returns on plants, but we do
@@ -18,6 +19,10 @@ const ReturnPolicy = () => {
           photo of your damaged plant ready. Then, let us know via the form
           below so we can ship a replacement for you as soon as possible.
         </p>
+        <h3>
+          Still have questions? We’re here to help!{" "}
+          <Link to="/login">Get in touch</Link> and we’ll get back to you as
+        </h3>
       </div>
     </div>
   );
