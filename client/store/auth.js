ff49c8bca3b5fece87181a11ea8314d31025f71c
Kokko Tso <kokkotso@gmail.com>
2021-02-20 10:32:39 -0500

    Fix redirect on hard reload issue - current location now persists

--- client/store/auth.js
+++ client/store/auth.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
-import history from '../history';
+import axios from "axios";
+import history from "../history";
 
 const storage = () => window.localStorage;
-const TOKEN = 'token';
+const TOKEN = "token";
 
 /**
  * ACTION TYPES
  */
-const SET_AUTH = 'SET_AUTH';
+const SET_AUTH = "SET_AUTH";
 
 /**
  * ACTION CREATORS
@@ -17,49 +17,52 @@ const setAuth = (auth) => ({ type: SET_AUTH, auth });
 /**
  * THUNK CREATORS
  */
-export const me = () => async (dispatch) => {
-  const token = storage().getItem(TOKEN);
-  if (token) {
-    //this response returns the user in our DB associated with the JWT token in localStorage
-    const res = await axios.get('/auth/me', {
-      headers: {
-        authorization: token,
-      },
-    });
-    console.log(history, 'history');
-    history.push('/');
-    return dispatch(setAuth(res.data));
-  }
+export const me = (method) => async (dispatch) => {
+	const token = storage().getItem(TOKEN);
+	if (token) {
+		//this response returns the user in our DB associated with the JWT token in localStorage
+		const res = await axios.get("/auth/me", {
+			headers: {
+				authorization: token,
+			},
+		});
+		if (method) {
+			history.push("/");
+		}
+		// console.log(history, "history");
+		// history.push("/");
+		return dispatch(setAuth(res.data));
+	}
 };
 
 export const authenticate = (email, password, method) => async (dispatch) => {
-  let res;
-  try {
-    res = await axios.post(`/auth/${method}`, { email, password });
-    storage().setItem(TOKEN, res.data.token);
-    dispatch(me());
-  } catch (authError) {
-    return dispatch(setAuth({ error: authError }));
-  }
+	let res;
+	try {
+		res = await axios.post(`/auth/${method}`, { email, password });
+		storage().setItem(TOKEN, res.data.token);
+		dispatch(me(method));
+	} catch (authError) {
+		return dispatch(setAuth({ error: authError }));
+	}
 };
 
 export const logout = () => {
-  // storage().removeItem(TOKEN);
-  storage().clear();
-  return {
-    type: SET_AUTH,
-    auth: {},
-  };
+	// storage().removeItem(TOKEN);
+	storage().clear();
+	return {
+		type: SET_AUTH,
+		auth: {},
+	};
 };
 
 /**
  * REDUCER
  */
 export default function (state = {}, action) {
-  switch (action.type) {
-    case SET_AUTH:
-      return action.auth;
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case SET_AUTH:
+			return action.auth;
+		default:
+			return state;
+	}
 }
